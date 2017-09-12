7414c0074b860f03333d1e57b63acd22d86b02d5
AlexGreen92 <alexgreen2015@icloud.com>
2017-09-12 07:25:37 -0500

    Lesson3.

--- scripts/samuraiScript.js
+++ scripts/samuraiScript.js
@@ -1,6 +1,2 @@
-var samuraiObj = angular
-						.module("myModule",[])
-						.controller("controller1",function($scope){var employeeObj = {firstName : "Alex",secondName : 'Green',gender : "Male"}
-						
-						$scope.employeeObj = employeeObj;
-});
+var samuraiObj = angular.module("myModule",[]).controller("controller1",function($scope){var employeeObj={firstName:"Alex",secondName:'Green',gender:"Male"}	
+$scope.employeeObj = employeeObj})
