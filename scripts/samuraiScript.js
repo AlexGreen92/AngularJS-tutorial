a821c014bbf2afe07815d354da40f79fa5d65270
Alex <AlexGreen2015@icloud.com>
2017-09-22 22:08:29 -0500

    Add files via upload
    7. commit

--- scripts/samuraiScript.js
+++ scripts/samuraiScript.js
@@ -1,9 +1,21 @@
 var samuraiObj = angular.module("myModule",[]).controller("controller1",function($scope){
-	var countries = [
-						{name:'USA',cities:[{name:'New York'},{name:'Chicago'},{name:'Denver'},{name:'Los Angeles'}]},
-						{name:'UK',cities:[{name:'London'},{name:'Manchester'},{name:'Binrmingham'},{name:'Liverpool'}]},
-						{name:'Canada',cities:[{name:'Toronto'},{name:'Montreal'},{name:'Vancouver'},{name:'Calgary'}]}
-	];
-$scope.countries = countries;})
+	var technologies = [  
 
-///var employees = [{firstName:"alex",secondName:'green',gender:'male',salary:'55000'},{firstName:"ben", secondName:'hastings',gender:'male',salary:'68000'},{firstName:"sara", secondName:'paul',gender:'female',salary:'57000'},{firstName:"mark", secondName:'holland',gender:'male',salary:'53000'},{firstName:"pam", secondName:'makintosh',gender:'female',salary:'60000'}];
+							{name:'C#',likes : 0, dislikes : 0},
+							{name:'ASP.NET',likes : 0, dislikes : 0},
+							{name:'SQL Server',likes : 0, dislikes : 0},
+							{name:'AngularJS',likes : 0, dislikes : 0}
+						];
+
+
+
+
+$scope.technologies = technologies;
+$scope.incrementLikes = function(technology){
+	technology.likes++;
+}
+$scope.incrementDislikes = function(technology){
+	technology.dislikes++;
+}
+})
+
