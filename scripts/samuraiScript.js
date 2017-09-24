555300053719238a8065193d516ecd61fff418f6
Alex <AlexGreen2015@icloud.com>
2017-09-24 02:14:49 -0500

    Add files via upload
    8.

--- scripts/samuraiScript.js
+++ scripts/samuraiScript.js
@@ -1,21 +1,15 @@
 var samuraiObj = angular.module("myModule",[]).controller("controller1",function($scope){
-	var technologies = [  
+	var employees = [  
 
-							{name:'C#',likes : 0, dislikes : 0},
-							{name:'ASP.NET',likes : 0, dislikes : 0},
-							{name:'SQL Server',likes : 0, dislikes : 0},
-							{name:'AngularJS',likes : 0, dislikes : 0}
+							{name:'Ben',dateOfBirth : new Date('November 23,1980'), gender : 'Male', salary : 57.777},
+							{name:'Sara',dateOfBirth : new Date('May 5,1970'), gender : 'Female', salary : 67.777},
+							{name:'Mark',dateOfBirth : new Date('August 15,1975'), gender : 'Male', salary : 77.777},
+							{name:'Pam',dateOfBirth : new Date('October 27, 1979'), gender : 'Female', salary : 47.777},
+							{name:'Todd',dateOfBirth : new Date('December 30, 1983'), gender : 'Male', salary : 78.777},
 						];
+$scope.employees = employees;
 
+$scope.rowLimit = 3;
 
-
-
-$scope.technologies = technologies;
-$scope.incrementLikes = function(technology){
-	technology.likes++;
-}
-$scope.incrementDislikes = function(technology){
-	technology.dislikes++;
-}
 })
 
