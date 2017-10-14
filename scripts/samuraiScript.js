a930240e5d8758c8a0ebd913eb1683a331f652b8
Alex <AlexGreen2015@icloud.com>
2017-10-14 03:16:22 -0500

    Add files via upload

--- scripts/samuraiScript.js
+++ scripts/samuraiScript.js
@@ -1,27 +1,11 @@
 var samuraiObj = angular.module("myModule",[])
 .controller("controller1",function($scope){
 	var employees = [ 
-						{name:'Ben',dateOfBirth : new Date('November 23,1980'), gender : 'Male', salary : 57.777},
-						{name:'Sara',dateOfBirth : new Date('May 5,1970'), gender : 'Female', salary : 67.777},
-						{name:'Mark',dateOfBirth : new Date('August 15,1975'), gender : 'Male', salary : 77.777},
-						{name:'Pam',dateOfBirth : new Date('October 27, 1979'), gender : 'Female', salary : 47.777},
-						{name:'Todd',dateOfBirth : new Date('December 30, 1983'), gender : 'Male', salary : 78.777},
+						{name:'Ben', gender : 'Male', salary : 57.777, city : 'London'},
+						{name:'Sara', gender : 'Female', salary : 67.777, city : 'Chennai'},
+						{name:'Mark', gender : 'Male', salary : 77.777, city : 'London'},
+						{name:'Pam', gender : 'Female', salary : 47.777, city : 'Chennai'},
+						{name:'Todd', gender : 'Male', salary : 78.777, city : 'London'},
 					];
 		$scope.employees = employees;
-		$scope.sortColumn='name';
-		$scope.reverseSort = false;
-
-		$scope.sortData = function (column) {
-			$scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
-			$scope.sortColumn = column;
-			console.log('hi');
-		}
-
-		$scope.getSortClass = function (column){
-			console.log('hey');
-			if($scope.sortColumn == column) {
-				return $scope.reverseSort ? 'arrow-down' : 'arrow-up';
-			}
-			return ;
-		}
 });
