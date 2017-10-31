835888750c797ddd0cda61572651eed34e6a913c
Alex <AlexGreen2015@icloud.com>
2017-10-31 02:12:23 -0500

    Add files via upload

--- scripts/samuraiScript.js
+++ scripts/samuraiScript.js
@@ -1,10 +1,13 @@
 var samuraiObj = angular
 .module("myModule",[])
 .controller("controller1",function($scope){
-	var countries = [ 
-						{name:'USA', cities:[{name:'Los Angeles'},{name:'Chicago'}]},
-						{name:'India', cities:[{name:'Hyderabad'},{name:'Chennai'}]},
-						
-					];
-		$scope.countries = countries;
+	var employees = [ 
+ 						{name:'Ben', gender : 'Male', salary : 57.777, city : 'London'},
+ 						{name:'Sara', gender : 'Female', salary : 67.777, city : 'Chennai'},
+ 						{name:'Mark', gender : 'Male', salary : 77.777, city : 'London'},
+ 						{name:'Pam', gender : 'Female', salary : 47.777, city : 'Chennai'},
+ 						{name:'Todd', gender : 'Male', salary : 78.777, city : 'London'}
+ 					]
+		$scope.employees = employees;
+		$scope.employeeView="employeeTable.html";
 });
